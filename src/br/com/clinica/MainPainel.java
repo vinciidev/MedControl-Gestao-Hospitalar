37f20ac1443e5e5a4b75788e6e4bbdb27f18b5b2
gabriellopeszdev <lpbiel4@gmail.com>
2025-09-21 23:34:31 -0300

    Cadastro de Usuarios

--- src/br/com/clinica/MainPainel.java
+++ src/br/com/clinica/MainPainel.java
@@ -136,7 +136,6 @@ public class MainPainel {
             cadastroFrame.setSize(450, 500);
             cadastroFrame.setLocationRelativeTo(mainFrame); // Centraliza em relação à tela principal
             cadastroFrame.setVisible(true);
-
         });
 
         prontuarioEButton.addActionListener(e -> {
